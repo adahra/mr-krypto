81eeb3fb07cdb1e6bc240a990ae81b128a691605
adnanto.ahmad <adnanto.ahmad@sebangsa.com>
2017-12-20 16:16:48 +0700

    Menambah config untuk enkrip dan dekrip

---
+++ app/src/main/java/io/github/adahra/mrkrypto/data/config/Dekrip.java
@@ -0,0 +1,9 @@
+package io.github.adahra.mrkrypto.data.config;
+
+/**
+ * Created on 12/20/17.
+ */
+
+public class Dekrip {
+
+}
---
+++ app/src/main/java/io/github/adahra/mrkrypto/data/config/Enkrip.java
@@ -0,0 +1,8 @@
+package io.github.adahra.mrkrypto.data.config;
+
+/**
+ * Created on 12/20/17.
+ */
+
+public class Enkrip {
+}
